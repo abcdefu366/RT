40c0af60b0d777f4bfa0e2d44d6bf51551bc4ef4
abcdefu366 <SmirnovIgor2005@yandex.ru>
2023-01-23 01:25:39 +0300

    done

--- src/Main.java
+++ src/Main.java
@@ -1,56 +1,77 @@
+import java.sql.SQLOutput;
 import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
-        Varen varen = new Varen("Варен");
-        Morel morel = new Morel("Морель");
-        Narrator Narrator = new Narrator();
-        Narrator.addPhrase(varen.getPhrase());
-        Narrator.addPhrase(morel.getPhrase());
-        Narrator.addPhrase(varen.getPhrase());
-        Narrator.addPhrase(morel.getPhrase());
-        Narrator.addPhrase(varen.getPhrase());
-        Narrator.addPhrase(morel.getPhrase());
-        Narrator.addPhrase(varen.getPhrase());
-        Narrator.addPhrase(morel.getPhrase());
-        Narrator.addPhrase(varen.getPhrase());
-        Narrator.sayPhrase();
-        Narrator.sayPhrase();
-        Narrator.sayPhrase();
-        Narrator.sayPhrase();
-        Narrator.sayPhrase();
-        Narrator.sayPhrase();
+        Mary mary = new Mary("Мэри");
+        mary.method();
     }
 }
-class Narrator {
-    private ArrayList<String> phrases = new ArrayList<>();
-    private int numberOfPhrase;
-    public void addPhrase(String phrase) {
-        phrases.add(numberOfPhrase, phrase);
-        numberOfPhrase++;
-    }
-    public void sayPhrase() {
-        int a = (int) Math.ceil(Math.random()*8);
-        System.out.println(phrases.get(a));
-    }
-}
-class Varen {
+class Mary {
     private String name;
-    private String phrase = "Это моя фраза!";
-    public String getPhrase() {
-        return phrase;
+    private double happiness = 0.5;
+    private String location;
+    private String status;
+
+    public String getLocation() {
+        return location;
     }
-    public Varen(String name) {
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public Mary(String name) {
         this.name = name;
     }
-}
-class Morel {
-    private String name;
-    private String phrase = "А это моя фраза!";
-    public String getPhrase() {
-        return phrase;
-    }
-    public Morel(String name) {
-        this.name = name;
+    public void method() {
+        double parameter = Math.random();
+        double a = 0.33;
+        double b = 0.66;
+        while (happiness > 0 && happiness < 1) {
+            System.out.println(parameter);
+            if (parameter < a) {
+                happiness += 0.08;
+                setLocation("Старые клумбы");
+                System.out.println(getName() + " подошла к " + getLocation());
+                String str = String.format("%.0f", happiness * 100);
+                System.out.println("Счастье " + getName() + " увеличилось на 8% и теперь равно: " + str + "%");
+            }
+            if (parameter >= a && parameter < b) {
+                happiness += 0.04;
+                setLocation("Заросшие лужайки");
+                System.out.println(getName() + " подошла к " + getLocation());
+                String str = String.format("%.0f", happiness * 100);
+                System.out.println("Счастье " + getName() + " увеличилось на 4% и теперь равно: " + str + "%");
+            }
+            if (parameter >= b) {
+                happiness -= 0.06;
+                setLocation("Края дорожек");
+                System.out.println(getName() + " подошла к " + getLocation());
+                String str = String.format("%.0f", happiness * 100);
+                System.out.println("Счастье " + getName() + " уменьшилось на 6% и теперь равно: " + str + "%");
+            }
+            parameter = Math.random();
+        }
+        if (happiness <= 0) {
+            setStatus("Грустная");
+            System.out.println("Теперь " + getName() + " " + getStatus() + ", и  она не может увидеть много зелёных ростков.");
+        }
+        if (happiness >= 1) {
+            setStatus("Весёлая");
+            System.out.println("Теперь " + getName() + " " + getStatus() + ", и  она может увидеть много зелёных ростков!");
+        }
     }
 }
